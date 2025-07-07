9ce6b731b72961577870940f97c17bf0efcfff63
vanshika-117 <75032921+vanshika-117@users.noreply.github.com>
2025-07-07 10:33:08 +0530

    Add files via upload

--- node_modules/set.js
+++ node_modules/set.js
@@ -1,35 +1,50 @@
 'use strict';
 
-var callBind = require('call-bind-apply-helpers');
-var gOPD = require('gopd');
-var $TypeError = require('es-errors/type');
+var test = require('tape');
 
-/** @type {{ __proto__?: object | null }} */
-var obj = {};
-try {
-	obj.__proto__ = null; // eslint-disable-line no-proto
-} catch (e) {
-	if (!e || typeof e !== 'object' || !('code' in e) || e.code !== 'ERR_PROTO_ACCESS') {
-		throw e;
+var setDunderProto = require('../set');
+
+test('setDunderProto', { skip: !setDunderProto }, function (t) {
+	if (!setDunderProto) {
+		throw 'should never happen; this is just for type narrowing'; // eslint-disable-line no-throw-literal
 	}
-}
 
-var hasProtoMutator = !('toString' in obj);
+	// @ts-expect-error
+	t['throws'](function () { setDunderProto(); }, TypeError, 'throws if no arguments');
+	// @ts-expect-error
+	t['throws'](function () { setDunderProto(undefined); }, TypeError, 'throws with undefined and nothing');
+	// @ts-expect-error
+	t['throws'](function () { setDunderProto(undefined, undefined); }, TypeError, 'throws with undefined and undefined');
+	// @ts-expect-error
+	t['throws'](function () { setDunderProto(null); }, TypeError, 'throws with null and undefined');
+	// @ts-expect-error
+	t['throws'](function () { setDunderProto(null, undefined); }, TypeError, 'throws with null and undefined');
 
-// eslint-disable-next-line no-extra-parens
-var desc = gOPD && gOPD(Object.prototype, /** @type {keyof typeof Object.prototype} */ ('__proto__'));
+	/** @type {{ inherited?: boolean }} */
+	var obj = {};
+	t.ok('toString' in obj, 'object initially has toString');
 
-/** @type {import('./set')} */
-module.exports = hasProtoMutator && (
-// eslint-disable-next-line no-extra-parens
-	(!!desc && typeof desc.set === 'function' && /** @type {import('./set')} */ (callBind([desc.set])))
-	|| /** @type {import('./set')} */ function setDunder(object, proto) {
-		// this is node v0.10 or older, which doesn't have Object.setPrototypeOf and has undeniable __proto__
-		if (object == null) { // eslint-disable-line eqeqeq
-			throw new $TypeError('set Object.prototype.__proto__ called on null or undefined');
-		}
-		// eslint-disable-next-line no-proto, no-param-reassign, no-extra-parens
-		/** @type {{ __proto__?: object | null }} */ (object).__proto__ = proto;
-		return proto;
+	setDunderProto(obj, null);
+	t.notOk('toString' in obj, 'object no longer has toString');
+
+	t.notOk('inherited' in obj, 'object lacks inherited property');
+	setDunderProto(obj, { inherited: true });
+	t.equal(obj.inherited, true, 'object has inherited property');
+
+	t.end();
+});
+
+test('no dunder proto', { skip: !!setDunderProto }, function (t) {
+	if ('__proto__' in Object.prototype) {
+		t['throws'](
+			// @ts-expect-error
+			function () { ({}).__proto__ = null; }, // eslint-disable-line no-proto
+			Error,
+			'throws when setting Object.prototype.__proto__'
+		);
+	} else {
+		t.notOk('__proto__' in Object.prototype, 'no __proto__ in Object.prototype');
 	}
-);
+
+	t.end();
+});
